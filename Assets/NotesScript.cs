d2981676302b902d57a4b2fa705c5be359937751
Shashank Sharma <shoutforpainkiller@gmail.com>
2022-09-18 03:31:03 +0530

    Temporary Note scripts
    Added a temporary notes script that attaches as a component, just to keep logs from my mind for the next time.

---
+++ Assets/NotesScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesScript : MonoBehaviour
+{
+    [TextArea]
+    public string Note = string.Empty;
+}
